32f90ce91ccbe04b772fbd3b5336751d489cabb3
klepininalex <klepininalex@gmail.com>
2023-07-23 23:36:52 +0500

    fix homework_3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,12 +1,12 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT customer.company_name, CONCAT(employee.first_name, ' ', employee.last_name)
-FROM orders AS fullname
-JOIN customers AS customer USING(customer_id)
-JOIN employees AS employee USING(employee_id)
-JOIN shippers AS shipper ON fullname.ship_via = shipper.shipper_id
-WHERE customer.city = 'London' AND employee.city = 'London' AND shipper.company_name = 'United Package';
+SELECT company_name, CONCAT(employees.first_name, ' ', employees.last_name)
+FROM orders
+JOIN employees USING (employee_id)
+JOIN shippers USING(shipper_id)
+JOIN customer USING(customer_id)
+WHERE orders.ship_city='London' and employees.city='London' AND company_name='United Package';
 
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
@@ -15,8 +15,8 @@ WHERE customer.city = 'London' AND employee.city = 'London' AND shipper.company_
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 SELECT product_name, units_in_stock, contact_name, phone
 FROM products
-JOIN categories USING(category_id)
 JOIN suppliers USING(supplier_id)
+JOIN categories USING(category_id)
 WHERE discontinued != 1 AND units_in_stock < 25 AND category_name IN ('Dairy Products', 'Condiments')
 ORDER BY units_in_stock;
 
@@ -30,4 +30,4 @@ WHERE order_id IS NULL;
 -- Этот запрос написать именно с использованием подзапроса.
 SELECT DISTINCT product_name
 FROM products
-WHERE product_id IN (SELECT product_id FROM order_details WHERE order_details.quantity=10);
+WHERE product_id EXISTS (SELECT product_id FROM order_details WHERE quantity=10)
